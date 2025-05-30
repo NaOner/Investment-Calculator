da63296bc47c26189e649a32eea665625b93696d
Kacper <Kacper@MacBook-Air.local>
2025-05-30 20:34:38 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Log from "./components/Log.jsx";
+
+
+function App() {
+  return (
+      <>
+          <Header />
+          <UserInput />
+      </>
+  )
+}
+
+export default App
